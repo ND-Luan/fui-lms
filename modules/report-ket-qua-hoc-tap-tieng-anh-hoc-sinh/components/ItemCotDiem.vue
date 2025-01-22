386589500a8a2ac65dc92a9add4659b7fdbed0fd
LuAn <63506861+ND-Luan@users.noreply.github.com>
2025-01-22 14:28:47 +0700

    update

--- modules/report-ket-qua-hoc-tap-tieng-anh-hoc-sinh/components/ItemCotDiem.vue
+++ modules/report-ket-qua-hoc-tap-tieng-anh-hoc-sinh/components/ItemCotDiem.vue
@@ -1,8 +1,22 @@
 <template>
-    <v-list-item :title="item?.TenCotDiem_VI">
+    <v-list-item :class="item.TenCotDiem_EN === 'Total' ? 'bg-primary' : ''">
         <template v-slot:append>
-            <v-chip color="primary" size="small" v-if="ketQuaDanhGia"> {{ ketQuaDanhGia }}</v-chip>
+            <v-chip  :color="item.TenCotDiem_EN === 'Total' ? 'white' : 'primary'" size="small" v-if="ketQuaDanhGia">
+                {{ ketQuaDanhGia }}
+            </v-chip>
         </template>
+        <v-list-item-title>
+            <p :class="item.TenCotDiem_EN === 'Total' ? 'font-weight-medium ' : ''">{{ item?.TenCotDiem_VI }}</p>
+        </v-list-item-title>
+        <v-list-item-subtitle>
+            Thang điểm:
+            <v-chip size="small">
+                {{ item.DiemMin }}
+            </v-chip>
+            <v-chip class="ml-1" size="small">
+                {{ item.DiemMax }}
+            </v-chip>
+        </v-list-item-subtitle>
     </v-list-item>
 </template>
 <script>
